6f6f5cc2decaac47935504a9a26b3586f23afca3
rorychatt <pavel.rinne@gmail.com>
2024-09-03 11:40:09 +0200

    Feat: project setup

---
+++ Калькулятор/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Калькулятор;
+
+public class Class1
+{
+
+}
---
+++ КалькуляторТесты/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace КалькуляторТесты;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
